f3d203e7e1aa1d7ff15424f896dfb0c055753c16
Tahmid Imran <tahmidimran1@gmail.com>
2021-01-26 21:56:53 -0600

    added main functionality

--- discover.py
+++ discover.py
@@ -2,13 +2,16 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from secret import client_id, client_secret, redirect_uri, weely_playlist_id, all_playlist_id
 
-scope = 'playlist-read-private'
+""" scope = 'playlist-read-private' """
+scope = 'playlist-modify-private'
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
 
 results = sp.playlist_items(
     playlist_id=weely_playlist_id)
-for idx, item in enumerate(results['items']):
+to_add = []
+for item in (results['items']):
     track = item['track']
-    print(idx, track['name'], " - ", track['artists'][0]['name'])
+    to_add.append(track['uri'])
+sp.playlist_add_items(playlist_id=all_playlist_id, items=to_add)
